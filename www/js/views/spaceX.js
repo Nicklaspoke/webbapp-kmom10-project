583ef92ee160e5675910b5d380e5a6021f11074d
Nicklaspoke <Nicklas.poke@gmail.com>
2019-06-04 19:46:22 +0200

    works don't touch 2, removed old feature

--- www/js/views/spaceX.js
+++ www/js/views/spaceX.js
@@ -12,12 +12,6 @@ let spaceX = {
     },
 
     view: function() {
-        spaceX.optionArray = [];
-
-        for (let i = 0; i < spaceXModel.latestLaunchId; i++) {
-            spaceX.optionArray[i] = i+1;
-        }
-
         return [
             m("div.container.slide-in", [
                 m("h1", "What do you wanna see?"),
@@ -32,29 +26,7 @@ let spaceX = {
                         m.route.set("/spaceXList/upcoming");
                     }
                 }, "Upcoming Launches"),
-                m("form", {
-                    onsubmit: function() {
-                        event.preventDefault();
-                        console.log(spaceXModel.selectedLaunchId);
-                        if (spaceXModel.selectedLaunchId != 0) {
-                            m.route.set("/spaceX/" + spaceXModel.selectedLaunchId);
-                        }
-                    }
-                }, [
-                    m("label.input-label", "Select A Specific Launch"),
-                    m("select.dropdown-menu", {
-                        onchange: function(e) {
-                            spaceXModel.selectedLaunchId = e.target.value;
-                            console.log(spaceXModel.selectedLaunchId);
-                        },
-                    }, [
-                        m("option.dropdown-menu", "Select a launch"),
-                        spaceX.optionArray.map(function(launchId) {
-                            return m("option", {value: launchId}, launchId);
-                        }),
-                    ]),
-                    m("input[type=submit][value=Get Launch Info].button.space-button.full-width-button", "Get Launch Info")
-                ]),
+
                 m("input[type=submit][value=Browse All Launches].button.space-button.full-width-button", {
                     onclick: function() {
                         m.route.set("/spaceXList/all");
